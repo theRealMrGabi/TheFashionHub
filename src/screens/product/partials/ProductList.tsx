15e5ad7a3d3f88fcc920b5a6622b2f03aac64182
therealmrgabi <gabirated@gmail.com>
2021-02-21 01:27:43 +0100

    product list page designed

--- src/screens/product/partials/ProductList.tsx
+++ src/screens/product/partials/ProductList.tsx
@@ -1,29 +1,22 @@
 import { FC } from "react";
-import { Card, ProductPageProducts } from "components";
-import { Radio, RadioGroup } from "@uiw/react-radio";
+import { Card, ProductPageProducts, SelectRadio } from "components";
+
+const data = [
+	{ value: "all", key: "All" },
+	{ value: "Nike", key: "Nike" },
+	{ value: "Nike", key: "Nike" },
+	{ value: "Adidas", key: "Adidas" },
+	{ value: "Puma", key: "Puma" },
+];
 
 export const ProductList: FC = () => {
 	return (
 		<div className="flex flex-col md:flex-row mt-12 w-full">
-			{/* <hr /> */}
-			{/* <div className="w-3/12 mx-auto md:mx-0">
-				<h4>By Designer</h4>
-				<RadioGroup className="flex flex-col">
-					<Radio value="all">All</Radio>
-					<Radio value="Nike">Nike</Radio>
-					<Radio value="Adidas">Adidas</Radio>
-					<Radio value="Fenti">All</Radio>
-				</RadioGroup>
-			</div> */}
 			<div className="w-3/12 mx-auto md:mx-0 mr-8 pr-16">
-				<hr />
-				<h4>By Designer</h4>
-				<RadioGroup className="flex flex-col text-r">
-					<Radio value="all">All</Radio>
-					<Radio value="Nike">Nike</Radio>
-					<Radio value="Adidas">Adidas</Radio>
-					<Radio value="Fenti">All</Radio>
-				</RadioGroup>
+				{/* <div onClick={handleVisibility}>
+					<Dropdown category="Brands" visible={visible} />
+				</div> */}
+				<SelectRadio category="Brands" selectData={data} />
 			</div>
 
 			<div className="w-9/12 cards flex flex-col-reverse md:flex-row items-center flex-wrap justify-between mx-auto md:mx-0">
