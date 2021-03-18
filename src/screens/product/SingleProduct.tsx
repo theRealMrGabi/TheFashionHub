69dc731ee9bfb24cee7a2e76af2a895be1ecb9b0
therealmrgabi <gabirated@gmail.com>
2021-03-18 01:30:27 +0100

    product props

--- src/screens/product/SingleProduct.tsx
+++ src/screens/product/SingleProduct.tsx
@@ -1,7 +1,11 @@
 import { FC } from "react";
 import { RouteComponentProps } from "react-router";
 
-const SingleProduct: FC<RouteComponentProps> = () => {
+const SingleProduct: FC<RouteComponentProps> = (props: any) => {
+	console.log("product props --->", props);
+	const id = props.match.params.id;
+	console.log("product id --->", id);
+
 	return (
 		<div>
 			<h3>SingleProduct</h3>
